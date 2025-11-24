5a041f4ac6560e23f2df5dedd19c9e41caeaa7df
preethadk11 <preethadk11@gmail.com>
2025-11-24 13:06:48 +0530

    converting two list and set into dictionary

---
+++ 25.py
@@ -0,0 +1,8 @@
+keys=["names","age"]
+values=["Preetha",34]
+my_dict=dict(zip(keys,values))
+print(my_dict)
+set1={"names","age"}
+set2={"Priyanga",45}
+my_dict2=dict(zip(list(set1),list(set2)))
+print(my_dict2)
